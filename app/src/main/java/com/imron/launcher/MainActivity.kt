ebf38cf4776631631d141625fa93b5e495b42ad7
BEREimron <emailkamu@example.com>
2026-09-02 23:36:45 +0800

    Initial commit

---
+++ app/src/main/java/com/imron/launcher/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.imron.launcher
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = "Selamat Datang di Imron Launcher"
+        tv.textSize = 24f
+        setContentView(tv)
+    }
+}
